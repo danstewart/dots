95d5384106d1550b3539559d0d7caca07972a1e8
Dan Stewart <git@mail.danstewart.dev>
2020-05-29 10:31:36 +0100

    sensible.bash changes

--- config/bash/sensible.bash
+++ config/bash/sensible.bash
@@ -31,18 +31,18 @@ bind Space:magic-space
 shopt -s globstar 2> /dev/null
 
 # Case-insensitive globbing (used in pathname expansion)
-shopt -s nocaseglob;
+#shopt -s nocaseglob;
 
 ## SMARTER TAB-COMPLETION (Readline bindings) ##
 
 # Perform file completion in a case insensitive fashion
-bind "set completion-ignore-case on"
+#bind "set completion-ignore-case on"
 
 # Treat hyphens and underscores as equivalent
 bind "set completion-map-case on"
 
 # Display matches for ambiguous patterns at first tab press
-bind "set show-all-if-ambiguous on"
+# bind "set show-all-if-ambiguous on"
 
 # Immediately add a trailing slash when autocompleting symlinks to directories
 bind "set mark-symlinked-directories on"
