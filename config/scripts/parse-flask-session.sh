24d904459523f47c2f495e96a42dfe8ee1f07147
Dan Stewart <git@mail.danstewart.dev>
2023-01-09 09:00:52 +0000

    Add script

---
+++ config/scripts/parse-flask-session.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+session="$1"
+
+if [[ -z $session ]]; then
+    echo "Please enter the flask session value from your browser cookie: "
+    read -r session
+fi
+
+# If the session starts with a dot, it's compressed
+if [[ $session =~ ^\..* ]]; then
+    payload=$(echo $session | cut -d. -f2)
+    python3 -c "import base64; import zlib; print(zlib.decompress(base64.urlsafe_b64decode('$payload')))"
+else
+    payload=$(echo $session | cut -d. -f1)
+    python3 -c "import base64; print(base64.urlsafe_b64decode('$payload'))"
+fi
