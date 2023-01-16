1a5f09d65a77cb6681062b89ad28d18b2911e7e3
Dan Stewart <git@mail.danstewart.dev>
2023-01-16 09:29:57 +0000

    Updates

--- config/scripts/parse-flask-session.sh
+++ config/scripts/parse-flask-session.sh
@@ -10,8 +10,10 @@ fi
 # If the session starts with a dot, it's compressed
 if [[ $session =~ ^\..* ]]; then
     payload=$(echo $session | cut -d. -f2)
+    payload="${payload}===="
     python3 -c "import base64; import zlib; print(zlib.decompress(base64.urlsafe_b64decode('$payload')))"
 else
     payload=$(echo $session | cut -d. -f1)
+    payload="${payload}===="
     python3 -c "import base64; print(base64.urlsafe_b64decode('$payload'))"
 fi
