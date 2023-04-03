d92115e37b64402517b0c8bf65c45d728b7503e3
Dan Stewart <git@mail.danstewart.dev>
2023-04-03 10:22:37 +0100

    Accept full filter format

--- config/scripts/docker-remove.sh
+++ config/scripts/docker-remove.sh
@@ -6,9 +6,10 @@ if [[ $pattern == "--help" || -z $pattern ]]; then
     echo "Usage: $0 <pattern>"
     echo ""
     echo "Removes all running docker containers matching the given pattern."
+    echo "See https://docs.docker.com/engine/reference/commandline/ps/#filter"
     exit 0
 fi
 
 echo "Removing docker containers matching $pattern..."
-docker container ls --filter name="$pattern" --format json | jq --raw-output '.Names' | xargs docker rm -f
+docker container ls --filter $pattern --format json | jq --raw-output '.Names' | xargs docker rm -f
 echo "Done."
