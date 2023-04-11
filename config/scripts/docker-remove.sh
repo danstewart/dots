2bb3b7f310474997b48b1b2c71913dc7cb7582a8
Dan Stewart <git@mail.danstewart.dev>
2023-04-11 14:14:04 +0100

    Update docker-remove script

--- config/scripts/docker-remove.sh
+++ config/scripts/docker-remove.sh
@@ -6,6 +6,11 @@ if [[ $pattern == "--help" || -z $pattern ]]; then
     echo "Usage: $0 <pattern>"
     echo ""
     echo "Removes all running docker containers matching the given pattern."
+    echo ""
+    echo "Examples"
+    echo "    # Delete all containers which contain 'xyz' in the name"
+    echo "    $0 name=xyz"
+    echo ""
     echo "See https://docs.docker.com/engine/reference/commandline/ps/#filter"
     exit 0
 fi
