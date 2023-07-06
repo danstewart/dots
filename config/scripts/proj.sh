25d737ae516bb9b6f9b4e4139aaef70ae23d752b
Dan Stewart <git@mail.danstewart.dev>
2023-07-06 08:43:03 +0100

    Tidy up proj.sh

--- config/scripts/proj.sh
+++ config/scripts/proj.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
+main() {
+	if [[ $1 == "ta" ]]; then
+		launch athena ~/tw/touramigo/athena-app
+		launch hermes ~/tw/touramigo/hermes-app
+		launch admin ~/tw/touramigo/admin-scripts
+	fi
 
-if [[ $1 == "ta" ]]; then
-	kitty @ launch --tab-title athena --type tab --cwd ~/tw/touramigo/athena-app
-	kitty @ launch --tab-title hermes --type tab --cwd ~/tw/touramigo/hermes-app
-	kitty @ launch --tab-title admin --type tab --cwd ~/tw/touramigo/admin-scripts
-fi
+	if [[ $1 == "ib" ]]; then
+		launch issuebear ~/tw/issuebear/issuebear
+		launch mgmt ~/tw/issuebear/issuebear-management
+	fi
+}
 
-if [[ $1 == "ib" ]]; then
-	kitty @ launch --tab-title issuebear --type tab --cwd ~/tw/issuebear/issuebear
-	kitty @ launch --tab-title mgmt --type tab --cwd ~/tw/issuebear/issuebear-management
-fi
+function launch() {
+	title="$1"
+	location="$2"
+
+	kitty @ launch --tab-title "$title" --type tab --cwd "$location" --keep-focus
+}
+
+main $@
