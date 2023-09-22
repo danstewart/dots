c934c2a9e93606d6ed92eb572f5259b99da6c80f
Dan Stewart <git@mail.danstewart.dev>
2023-09-22 12:01:07 +0100

    Fix diff-so-fancy installer

--- installers/diff-so-fancy.sh
+++ installers/diff-so-fancy.sh
@@ -3,5 +3,7 @@
 set -e
 
 [[ ! -d ~/bin ]] && mkdir ~/bin
-curl --silent https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -o ~/bin/diff-so-fancy
+[[ ! -d ~/bin/lib ]] && mkdir ~/bin/lib
+curl --silent https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/diff-so-fancy -o ~/bin/diff-so-fancy
+curl --silent https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/lib/DiffHighlight.pm -o ~/bin/lib/DiffHighlight.pm
 chmod 0755 ~/bin/diff-so-fancy
