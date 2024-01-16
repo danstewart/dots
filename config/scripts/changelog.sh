285193853edd0cc9cd1f7940c22ab0c31f1201f4
Dan Stewart <git@mail.danstewart.dev>
2024-01-16 10:17:15 +0000

    Fix bugs in changelog.sh

--- config/scripts/changelog.sh
+++ config/scripts/changelog.sh
@@ -9,12 +9,14 @@
 set -euo pipefail
 
 help=0
+simple=0
 project=""
 since=""
 while [[ "$#" -gt 0 ]]; do
 	case "$1" in
 		--project) project=$2; shift ;;
         --since) since=$2; shift ;;
+        --simple) simple=1 ;;
 		-h|--help) help=1 ;;
 		--) shift; break ;;
 	esac
@@ -28,7 +30,8 @@ if [[ $help == 1 || -z $project || -z $since ]]; then
     echo "Generates a changelog from git history"
     echo ""
     echo "--project: The path to the git project locally"
-    echo "--since: The commit to start the changelog from"
+    echo "--since:   The commit to start the changelog from"
+    echo "--simple:  Only print out the change commit lines"
 
     exit 1
 fi
@@ -50,7 +53,12 @@ re_shortcut_link="(https:\/\/app\.shortcut\.com\/[a-z0-9-]+\/story\/[0-9]+)"
 # Go through each log row and format as markdown
 formatted=()
 while IFS= read -r line; do
-    formatted+=("## $line\n")
+    if [[ $simple == 1 ]]; then
+        echo "$line"
+        continue
+    fi
+
+    formatted+=("## $line\n\n")
 
     if [[ "$line" =~ ${re_commit_hash} ]]; then
         formatted+=("Commit: \`#${BASH_REMATCH[1]}\`  \n")
@@ -83,4 +91,4 @@ while IFS= read -r line; do
     formatted+=("\n---\n\n")
 done <<< "$rows"
 
-echo -e "${formatted[*]}"
+echo -e "${formatted[*]}" | perl -p -e 's/^\s+([^\s])/$1/g'
