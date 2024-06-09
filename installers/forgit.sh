4df67d1bece676c6d97d1abfafc13835d800ffbf
Dan Stewart <git@mail.danstewart.dev>
2024-06-09 13:52:04 +0100

    Fix forgit install

--- installers/forgit.sh
+++ installers/forgit.sh
@@ -6,6 +6,6 @@ set -e
 
 [[ ! -d ~/bin ]] && mkdir ~/bin
 
-curl --silent https://raw.githubusercontent.com/wfxr/forgit/master/forgit.plugin.sh -o ~/.forgit.bash
+curl --silent https://raw.githubusercontent.com/wfxr/forgit/master/forgit.plugin.zsh -o ~/.forgit.bash
 curl --silent https://raw.githubusercontent.com/wfxr/forgit/master/bin/git-forgit -o ~/bin/git-forgit
 chmod 0755 ~/bin/git-forgit
