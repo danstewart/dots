15d7bdcb5d07df645b0561b7550e5f1f9dbf13d7
Dan Stewart <git@mail.danstewart.dev>
2025-01-21 12:52:21 +0000

    Update atuin config

--- config/scripts/global_id.sh
+++ config/scripts/global_id.sh
@@ -5,6 +5,11 @@ if [[ $# == 1 ]]; then
     printf $1 | base64 --decode
 
 # Encode
-else
+elif [[ $# == 2 ]]; then
     printf "$1:$2" | base64
+
+else
+    echo "Usage:"
+    echo "   global_id.sh <ModelName> <ID>        Encodes a model name and ID into a global ID"
+    echo "   global_id.sh <GlobalID>              Decodes a global ID into a model name and ID"
 fi
